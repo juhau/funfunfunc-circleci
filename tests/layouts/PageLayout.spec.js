ea2068baf88121ee67cb8e50893a166a2e6b67f0
julien Hautier <julien.hautier@izifret.com>
2018-10-10 14:53:21 +0200

    repair the breaking test

--- tests/layouts/PageLayout.spec.js
+++ tests/layouts/PageLayout.spec.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme'
 
 describe('(Layout) PageLayout', () => {
   it('renders as a <div>', () => {
-    shallow(<PageLayout />).should.have.tagName('p')
+    shallow(<PageLayout />).should.have.tagName('div')
   })
 
   it('renders a project title', () => {
