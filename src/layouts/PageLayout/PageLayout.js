6bfcd1cda316b67461c17b31250482fa1e802a11
julien Hautier <julien.hautier@izifret.com>
2018-10-10 15:15:42 +0200

    fix eslint error

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -3,7 +3,7 @@ import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
-export const PageLayout = ({children}) => (
+export const PageLayout = ({ children }) => (
   <div className='container text-center'>
     <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
